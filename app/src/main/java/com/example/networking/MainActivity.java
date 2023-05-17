dadee2d9a575d57104379cd383bf74de9c8bb8b7
Zepronex <callesvensson@hotmail.se>
2023-05-17 17:42:00 +0200

    Modified AndroidManifest.xml to allow access for internet, changed gradle version and edited MainActivity.java to work with JSON data to display names of mountains in the RecyclerView.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,35 +4,62 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
-    private final String JSON_FILE = "mountains.json";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
 
     private ArrayList<Mountain> mountainList = new ArrayList<>();
-    private RecyclerView.Adapter adapter;
+
+    private RecyclerView recyclerView;
+
+    private MountainAdapter mountainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        recyclerView = findViewById(R.id.recyclerView);
+        mountainAdapter = new MountainAdapter(mountainList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mountainAdapter);
+
+        new JsonTask(this).execute(JSON_URL);
+        new JsonFile(this, this).execute("mountains.json");
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
-        // Here, you can update the mountainList with the downloaded JSON data
-        // For example:
-        // mountainList = parseJsonData(json);
-        // Remember to call adapter.notifyDataSetChanged() after updating the list to reflect the changes in the RecyclerView
-        // adapter.notifyDataSetChanged();
+        if (json != null) {
+            try {
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String name = jsonObject.getString("name");
+                    String ID = jsonObject.getString("ID");
+                    String type = jsonObject.getString("type");
+                    String company = jsonObject.getString("company");
+                    String location = jsonObject.getString("location");
+                    String category = jsonObject.getString("category");
+                    int size = jsonObject.getInt("size");
+                    int cost = jsonObject.getInt("cost");
+                    mountainList.add(new Mountain(ID, name, type, company, location, category, size, cost));
+                }
+                mountainAdapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
